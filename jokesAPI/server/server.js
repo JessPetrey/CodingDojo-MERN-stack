9c18172812b637f4747203e9995bb100c2a63233
Jessica Petrey <jesipetrey@gmail.com>
2023-02-18 14:16:12 -0800

    updated server.js

--- jokesAPI/server/server.js
+++ jokesAPI/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+require('./configs/mongoose.configs')
 app.use(express.json(), express.urlencoded({extended: true}))
+
+const Routes = require('./routes/jokes.routes')
+Routes(app)
 
 app.listen(8000, () => console.log('Listening on 8000'))
