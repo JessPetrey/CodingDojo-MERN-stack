c5db825310bbfb25d570785379abf7859f259dd3
Jessica Petrey <jesipetrey@gmail.com>
2023-02-19 21:26:22 -0800

    created new view

--- fullStackMERN/helloWorld/client/src/views/Main.js
+++ fullStackMERN/helloWorld/client/src/views/Main.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PersonForm from '../components/PersonForm';
+import axios from 'axios'
+import PersonList from '../components/PersonList';
 
-export default () => {
-    return (
+const Main = (props) => {
+    const [people, setPeople] = useState([])
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/people')
+            .then(res=>{
+                setPeople(res.data)
+                setLoaded(true)
+            })
+            .catch(err => console.error(err))
+}, [])
+    return(
         <div>
             <PersonForm />
+            <hr />
+            {loaded && <PersonList people={people}/>}
         </div>
     )
 }
+
+export default Main
