540a04d030ced1a39c85351cbb26e8e4bd995a37
Jessica Petrey <jesipetrey@gmail.com>
2023-02-20 17:42:18 -0800

    PM3 init, added needed routes to server

---
+++ productManager3/client/src/views/Detail.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import {useParams, useNavigate} from "react-router-dom";
+
+const Detail = () => {
+    const { id } = useParams()
+    // const nav = useNavigate()
+    const [oneProduct, setOneProduct] = useState([])
+
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/products/${id}`)
+            .then(res => {
+                console.log(res.data)
+                setOneProduct(res.data)
+            })
+            .catch(err => console.error(err))
+    }, [])
+    return (
+        <>
+            {
+                oneProduct ?
+                    <div>
+                        <h1>{oneProduct.title}</h1>
+                        <p>Price: ${oneProduct.price}</p>
+                        <p>Description: {oneProduct.description}</p>
+                    </div>
+                    : <h1>loading...</h1>
+
+            }
+        </>
+    )
+}
+
+export default Detail
