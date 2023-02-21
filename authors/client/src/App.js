1e2c4bca29991f3b4d9f965d43c645dd92dc070c
Jessica Petrey <jesipetrey@gmail.com>
2023-02-21 11:28:24 -0800

    add basic view files, adjust index and app.js

--- authors/client/src/App.js
+++ authors/client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from './views/Main'
+import Detail from './views/Detail';
+import Edit from './views/Edit';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<Main />} path='/authors' />
+        <Route element={<Detail />} path='/authors/:id' />
+        <Route element={<Edit />} path='/authors/:id/edit' />
+      </Routes>
     </div>
   );
 }
