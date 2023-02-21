a6005c3f239079a3227f129b00bd6c3314aada4a
Jessica Petrey <jesipetrey@gmail.com>
2023-02-21 11:23:27 -0800

    finished routes and model

--- authors/server/controllers/author.controller.js
+++ authors/server/controllers/author.controller.js
@@ -5,3 +5,41 @@ module.exports.index = (req, res) => {
         message: 'Hello World'
     })
 }
+
+module.exports.createAuthor = (req, res) => {
+    const {name} = req.body
+    Author.create({ name })
+    .then(author => res.json(author))
+    .catch(err => res.status(400).json(err))
+}
+
+module.exports.getAllAuthors = (req, res) => {
+    Author.find({})
+    .then(author => res.json(author))
+    .catch(err => res.status(400).json(err))
+}
+
+module.exports.getAuthor = (req, res) => {
+    Author.findOne({_id: req.params.id})
+        .then(author => res.json(author))
+        .catch(err => res.status(400).json(err))
+}
+
+module.exports.updateAuthor = (req, res) => {
+    Author.findOneAndUpdate(
+        {_id: req.params.id},
+        req.body,
+        {
+            new:true,
+            runValidators: true
+        }
+        )
+        .then(author => res.json(author))
+        .catch(err => res.status(400).json(err))
+}
+
+module.exports.deleteAuthor = (req, res) => {
+    Author.deleteOne({_id: req.params.id})
+        .then(author => res.json(author))
+        .catch(err => res.status(400).json(err))
+}
